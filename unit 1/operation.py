656c0dbcd0907e5bc3bf757b37b0a865e582692f
rob46 <rwalker070@boyslatin.net>
2026-09-09 15:11:39 +0000

    class notes

---
+++ unit 1/operation.py
@@ -0,0 +1,21 @@
+# Asignment operations-eclusively used to
+# assign values to variables.
+# think key/ value pairings
+
+# We use a single equal sign to represent the assignment
+# operator
+name = "Robert"
+grade = 10
+school = True
+
+# Arithmetic operator-used on numerical
+# data types to perform calculations.
+# intergers(whole numbers) and floats (decimal numbers)
+
+# print is a function that lets us show code
+# in the terminal
+print(3 + 3) # addition operator
+print(3.9- 3) # subtraction operater
+print(12 / 3) # division operator
+print(3 * 4) # multiplication operator
+
