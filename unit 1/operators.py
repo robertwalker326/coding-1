ff887ad85ee0d49111c89acf87a473ae1c7bb86d
rob46 <rwalker070@boyslatin.net>
2026-09-10 15:19:47 +0000

    work

---
+++ unit 1/operators.py
@@ -0,0 +1,56 @@
+# Asignment operations-eclusively used to
+# assign values to variables.
+# think key/ value pairings
+
+# We use a single equal sign to represent the assignment
+# operator
+name = "Robert"
+grade = 10
+school = True
+
+# Arithmetic operator-used on numerical
+# data types to perform calculations.
+# intergers(whole numbers) and floats (decimal numbers)
+
+# print is a function that lets us show code
+# in the terminal
+print(3 + 3) # addition operator
+print(3.9- 3) # subtraction operater
+print(12 / 3) # division operator
+print(3 * 4) # multiplication operator
+
+# comparison operators - set of symbols used
+# to assess if data is the same or different and 
+# how they differ
+
+print(10 > 1) # greater than operator
+print(2000 < 100) # less than operator
+
+# 2 equal signs compare if somet
+print("book" == "Book") # same as (FALSE)
+print("2" == 2  ) # same as (false
+print(2.0 == 2)
+
+# not equal is written with !      
+# this is to check and filter for values that are not
+# the same
+# side note - exclamtion ALWAYS means NOT in
+# programming
+print(200 != 100)
+print(300 !+300)
+
+
+# logical operators-compares 2 conditions to check if
+# they are true or false
+# conditions = other operations
+# instead pf symbols,we represent these with words:
+# and,or,not
+
+# AND - checks if 2 conditions are true.if yes,
+# result is true
+print(3 > 1 and 100 == 50) # this would come out to be true
+
+# OR - the "opposite day" operator.it will reverse the
+# result of the logical operators
+print(not(3 >1 and 100 >500))
+# this would come out to be false
