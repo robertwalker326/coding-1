93367d90b49fc1753bd63da06207bce4a6fd87b1
rob46 <rwalker070@boyslatin.net>
2026-09-22 15:09:35 +0000

    class notes

--- unit 1/operatorsActivty.py
+++ unit 1/operatorsActivty.py
@@ -2,3 +2,27 @@ print(200 == 100) # false
 print(15 > 18) # false
 print("coding1" == "coding") # false
 print("0" != 0) # true
+book = 10.99
+tablet = 399.99
+
+print(book + tablet)
+
+cart = 98.87
+amounttogetdiscount = 100.00
+
+print(cart > amounttogetdiscount)
+
+shoes = 200.00
+Tax = 0.07
+
+print(shoes * Tax)
+
+Gpa = 86
+Recommendation = True
+
+print(Gpa > 85 and Recommendation == True)
+
+parentContact1 = False
+parentContact2 = True
+
+print(prarentContact1 == True or parentContact2 == True)
---
+++ unit 2/bultinFunctions.py
@@ -0,0 +1,33 @@
+# Function - simply put; a code block of
+# instructions for computers to follow.
+
+# Built- in Function - a code block of
+# instruction for a computer to follow
+# that was already written for us.
+# PRE-WRITTEN CODE INSTRUCTIONS
+
+# Data bcasting functions
+# these are built in functions (pre-written instructions)
+# that change data types from one form into another
+# str() - this datacasting function allows you to change 
+# any data type into a string
+
+year = 1906
+print("This event took place in"+ str(year))
+
+# string concatenation - combining string data types
+# with one another. We use the plus sign (+) operator
+# to combine strings together
+
+print(3+3) # Adding 2 interger
+print("ten"+"4" +"7") # COMBINING 3 stri9ngs
+print("good" + "bye")
+
+# int()- Any date type passed into the brackets will
+# # be converted into a interger(whole number)
+
+num1 = input("type in a number: ") #input alaways returns a string
+print(4 + int(num1))
+
+
+
